811c27672516d0fb2171668fc6a6c08e55164714
김용담 <dambang0323@naver.com>
2017-11-04 21:24:07 +0900

    2017-11-04 cs231n assignment 정리

--- Assignment1/cs231n/classifiers/k_nearest_neighbor.py
+++ Assignment1/cs231n/classifiers/k_nearest_neighbor.py
@@ -10,33 +10,33 @@ class KNearestNeighbor(object):
 
     def train(self, X, y):
         """
-        Train the classifier. For k-nearest neighbors this is just
-        memorizing the training data.
+    Train the classifier. For k-nearest neighbors this is just
+    memorizing the training data.
 
-        Inputs:
-        - X: A numpy array of shape (num_train, D) containing the training data
-          consisting of num_train samples each of dimension D.
-        - y: A numpy array of shape (N,) containing the training labels, where
-             y[i] is the label for X[i].
-        """
+    Inputs:
+    - X: A numpy array of shape (num_train, D) containing the training data
+      consisting of num_train samples each of dimension D.
+    - y: A numpy array of shape (N,) containing the training labels, where
+         y[i] is the label for X[i].
+    """
         self.X_train = X
         self.y_train = y
 
     def predict(self, X, k=1, num_loops=0):
         """
-        Predict labels for test data using this classifier.
+    Predict labels for test data using this classifier.
 
-        Inputs:
-        - X: A numpy array of shape (num_test, D) containing test data consisting
-             of num_test samples each of dimension D.
-        - k: The number of nearest neighbors that vote for the predicted labels.
-        - num_loops: Determines which implementation to use to compute distances
-          between training points and testing points.
+    Inputs:
+    - X: A numpy array of shape (num_test, D) containing test data consisting
+         of num_test samples each of dimension D.
+    - k: The number of nearest neighbors that vote for the predicted labels.
+    - num_loops: Determines which implementation to use to compute distances
+      between training points and testing points.
 
-        Returns:
-        - y: A numpy array of shape (num_test,) containing predicted labels for the
-          test data, where y[i] is the predicted label for the test point X[i].
-        """
+    Returns:
+    - y: A numpy array of shape (num_test,) containing predicted labels for the
+      test data, where y[i] is the predicted label for the test point X[i].
+    """
         if num_loops == 0:
             dists = self.compute_distances_no_loops(X)
         elif num_loops == 1:
@@ -50,21 +50,21 @@ class KNearestNeighbor(object):
 
     def compute_distances_two_loops(self, X):
         """
-        Compute the distance between each test point in X and each training point
-        in self.X_train using a nested loop over both the training data and the
-        test data.
+    Compute the distance between each test point in X and each training point
+    in self.X_train using a nested loop over both the training data and the
+    test data.
 
-        Inputs:
-        - X: A numpy array of shape (num_test, D) containing test data.
+    Inputs:
+    - X: A numpy array of shape (num_test, D) containing test data.
 
-        Returns:
-        - dists: A numpy array of shape (num_test, num_train) where dists[i, j]
-          is the Euclidean distance between the ith test point and the jth training
-          point.
-        """
+    Returns:
+    - dists: A numpy array of shape (num_test, num_train) where dists[i, j]
+      is the Euclidean distance between the ith test point and the jth training
+      point.
+    """
         num_test = X.shape[0]
         num_train = self.X_train.shape[0]
-        dists = np.zeros((num_test, num_train)) #Make matrix num_test by num_train with 0
+        dists = np.zeros((num_test, num_train))  # Make matrix num_test by num_train with 0
 
         for i in xrange(num_test):
             for j in xrange(num_train):
@@ -74,9 +74,9 @@ class KNearestNeighbor(object):
                 # training point, and store the result in dists[i, j]. You should   #
                 # not use a loop over dimension.                                    #
                 #####################################################################
-                #dist = np.linalg.norm(num_test - num_train)
-                #dists[i, j] = np.linalg.norm(self.X_train - X)
-                dists[i, j] = np.sqrt(np.sum(X[i] - self.X_train[j])**2)
+                # dist = np.linalg.norm(num_test - num_train)
+                # dists[i, j] = np.linalg.norm(self.X_train - X)
+                dists[i, j] = np.sqrt(np.sum((X[i] - self.X_train[j]) ** 2))
 
                 #####################################################################
                 #                       END OF YOUR CODE                            #
@@ -85,11 +85,11 @@ class KNearestNeighbor(object):
 
     def compute_distances_one_loop(self, X):
         """
-        Compute the distance between each test point in X and each training point
-        in self.X_train using a single loop over the test data.
+    Compute the distance between each test point in X and each training point
+    in self.X_train using a single loop over the test data.
 
-        Input / Output: Same as compute_distances_two_loops
-        """
+    Input / Output: Same as compute_distances_two_loops
+    """
         num_test = X.shape[0]
         num_train = self.X_train.shape[0]
         dists = np.zeros((num_test, num_train))
@@ -100,7 +100,7 @@ class KNearestNeighbor(object):
             # points, and store the result in dists[i, :].                        #
             #######################################################################
 
-            dists[i] = np.sqrt(np.sum((X[i] - self.X_train)**2, axis=1))
+            dists[i] = np.sqrt(np.sum((X[i] - self.X_train) ** 2, axis=1))
 
 
             #######################################################################
@@ -110,11 +110,11 @@ class KNearestNeighbor(object):
 
     def compute_distances_no_loops(self, X):
         """
-        Compute the distance between each test point in X and each training point
-        in self.X_train using no explicit loops.
+    Compute the distance between each test point in X and each training point
+    in self.X_train using no explicit loops.
 
-        Input / Output: Same as compute_distances_two_loops
-        """
+    Input / Output: Same as compute_distances_two_loops
+    """
         num_test = X.shape[0]
         num_train = self.X_train.shape[0]
         dists = np.zeros((num_test, num_train))
@@ -131,9 +131,9 @@ class KNearestNeighbor(object):
         #       and two broadcast sums.                                         #
         #########################################################################
 
-        dists = np.sqrt((X**2).sum(axis=1, keepdims=True) + (self.X_train**2).sum(axis=1) - 2 * X.dot(self.X_train.T))
+        dists = np.sqrt((X ** 2).sum(axis=1, keepdims=True) + (self.X_train ** 2).sum(axis=1) - 2 * X.dot(self.X_train.T))
 
-        #(a-b)^2 = a^2 + b^2 - 2ab    //expansion
+        # (a-b)^2 = a^2 + b^2 - 2ab    //expansion
 
         #########################################################################
         #                         END OF YOUR CODE                              #
@@ -142,17 +142,17 @@ class KNearestNeighbor(object):
 
     def predict_labels(self, dists, k=1):
         """
-        Given a matrix of distances between test points and training points,
-        predict a label for each test point.
+    Given a matrix of distances between test points and training points,
+    predict a label for each test point.
 
-        Inputs:
-        - dists: A numpy array of shape (num_test, num_train) where dists[i, j]
-          gives the distance betwen the ith test point and the jth training point.
+    Inputs:
+    - dists: A numpy array of shape (num_test, num_train) where dists[i, j]
+      gives the distance betwen the ith test point and the jth training point.
 
-        Returns:
-        - y: A numpy array of shape (num_test,) containing predicted labels for the
-          test data, where y[i] is the predicted label for the test point X[i].
-        """
+    Returns:
+    - y: A numpy array of shape (num_test,) containing predicted labels for the
+      test data, where y[i] is the predicted label for the test point X[i].
+    """
         num_test = dists.shape[0]
         y_pred = np.zeros(num_test)
         for i in xrange(num_test):
---
+++ Assignment1/cs231n/classifiers/linear_svm.py
@@ -0,0 +1,128 @@
+import numpy as np
+from random import shuffle
+from past.builtins import xrange
+
+def svm_loss_naive(W, X, y, reg):
+  """
+  Structured SVM loss function, naive implementation (with loops).
+
+  Inputs have dimension D, there are C classes, and we operate on minibatches
+  of N examples.
+
+  Inputs:
+  - W: A numpy array of shape (D, C) containing weights.
+  - X: A numpy array of shape (N, D) containing a minibatch of data.
+  - y: A numpy array of shape (N,) containing training labels; y[i] = c means
+    that X[i] has label c, where 0 <= c < C.
+  - reg: (float) regularization strength
+
+  Returns a tuple of:
+  - loss as single float
+  - gradient with respect to weights W; an array of same shape as W
+  """
+  dW = np.zeros(W.shape) # initialize the gradient as zero
+
+  # compute the loss and the gradient
+  num_classes = W.shape[1]
+  num_train = X.shape[0]
+  loss = 0.0
+  for i in xrange(num_train):
+    scores = X[i].dot(W)
+    correct_class_score = scores[y[i]]
+    for j in xrange(num_classes):
+      if j == y[i]:
+        continue
+      margin = scores[j] - correct_class_score + 1 # note delta = 1
+      if margin > 0:
+        loss += margin
+        dW[:, j] += X[i].T
+        dW[:, y[i]] += X[i].T
+
+  # Right now the loss is a sum over all training examples, but we want it
+  # to be an average instead so we divide by num_train.
+  loss /= num_train
+  dW /= num_train
+
+  # Add regularization to the loss.
+  loss += reg * np.sum(W * W)
+  dW += reg * W
+
+  #############################################################################
+  # TODO:                                                                     #
+  # Compute the gradient of the loss function and store it dW.                #
+  # Rather that first computing the loss and then computing the derivative,   #
+  # it may be simpler to compute the derivative at the same time that the     #
+  # loss is being computed. As a result you may need to modify some of the    #
+  # code above to compute the gradient.                                       #
+  #############################################################################
+
+
+
+  return loss, dW
+
+
+def svm_loss_vectorized(W, X, y, reg):
+  """
+  Structured SVM loss function, vectorized implementation.
+
+  Inputs and outputs are the same as svm_loss_naive.
+  """
+  loss = 0.0
+  dW = np.zeros(W.shape) # initialize the gradient as zero
+
+  #############################################################################
+  # TODO:                                                                     #
+  # Implement a vectorized version of the structured SVM loss, storing the    #
+  # result in loss.                                                           #
+  #############################################################################
+  num_classes = W.shape[1]
+  num_train = X.shape[0]
+  scores = X.dot(W)
+
+  #correct_class_score = scores[y, np.arange(num_train)]
+  correct_class_score = scores[np.arange(num_train), y]
+  margins = scores - correct_class_score[:, np.newaxis] + 1.0
+  #margins[y, np.arange(num_train)] = 0
+  margins[np.arange(num_train), y] = 0
+
+  #max = np.maximum(np.zeros((num_classes, num_train)), margins)
+  max = np.maximum(np.zeros((num_train, num_classes)), margins)
+
+  loss = np.sum(max) / num_train
+  loss += 0.5 * reg * np.sum(W * W)
+
+
+  #############################################################################
+  #                             END OF YOUR CODE                              #
+  #############################################################################
+
+
+  #############################################################################
+  # TODO:                                                                     #
+  # Implement a vectorized version of the gradient for the structured SVM     #
+  # loss, storing the result in dW.                                           #
+  #                                                                           #
+  # Hint: Instead of computing the gradient from scratch, it may be easier    #
+  # to reuse some of the intermediate values that you used to compute the     #
+  # loss.                                                                     #
+  #############################################################################
+  fully_vecX = np.zeros(margins.shape)
+
+  #for i in len(fully_vecX) :
+  #  if margins[i] > 0 :
+  #    fully_vecX[i] = 1
+  fully_vecX[margins > 0] = 1
+  fully_vecX[xrange(num_train), list(y)] = 0
+  falses = np.sum(fully_vecX, axis=1)
+  fully_vecX[xrange(num_train), list(y)] = -falses
+
+  dW = (X.T).dot(fully_vecX)
+  dW /= num_train
+  dW += reg * W
+
+
+  #############################################################################
+  #                             END OF YOUR CODE                              #
+  #############################################################################
+
+  return loss, dW
